3390add5d60c853118ead81aff1a2148ea60076e
leonardo <leodsmf@gmail.com>
2025-02-04 15:39:26 -0300

    estilos globais

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { breakpoints } from "./breakpoints";
+import { fonts } from "./fonts";
+
+export const theme = {
+    COLORS: {
+
+        primaryColor: '#8A42DB',
+        primaryColor400: '#8A42DB',
+        secondaryColor: '#7DFFAF',
+        
+        surfaceBackground: '#171021',
+        surfacePrimary: '#2C243B',
+        surfaceSecondary: '#413A4F',
+        surfaceTertiary: '#4E4563',
+        
+        textPrimary: '#F5F6F6',
+        textSecondary: '#CDD0D4'
+    },
+    fonts,
+    breakpoints
+}
