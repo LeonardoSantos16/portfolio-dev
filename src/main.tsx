fd89142d0b789fffb110d1977393dfb7dea32227
leonardo.mendes <leodsmf@gmail.com>
2025-03-02 12:00:42 -0300

    rotas da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme.ts'
 import { StyleGlobal } from './styles/global.ts'
 import { BrowserRouter } from 'react-router-dom'
-import Home from './pages/home/index.tsx'
-import Project from './pages/projects/index.tsx'
 import Layout from './components/layout/index.tsx'
+import Header from './components/header/index.tsx'
+import Footer from './components/footer/index.tsx'
+import { AppRoutes } from './routes/index.tsx'
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <StrictMode>
-          <Project />
+          <Header />
+          <AppRoutes />
+          <Footer />
       </StrictMode>
       <StyleGlobal />
     </ThemeProvider>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,15 @@
 import About from "../../components/about-section";
 import Contact from "../../components/contact";
-import Footer from "../../components/footer";
-import Header from "../../components/header";
 import Headline from "../../components/headline";
 import ProjectsSection from "../../components/project-section";
 
 const Home = () => {
     return(
         <>
-            <Header />
             <Headline />
             <About/>
             <ProjectsSection />
             <Contact />
-            <Footer />
         </>
     )
 }
