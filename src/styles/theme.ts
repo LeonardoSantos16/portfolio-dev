775f92be08592ff4c2b0546b2c0ba954232584cb
leonardo.mendes <leodsmf@gmail.com>
2025-06-23 21:03:34 -0300

    tipando temas

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -11,7 +11,7 @@ export const theme = {
     surfacePrimary: "#2C243B",
     surfaceSecondary: "#413A4F",
     surfaceTertiary: "#4E4563",
-
+    gradientHeading: "linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%)",
     textPrimary: "#F5F6F6",
     textSecondary: "#CDD0D4",
     white: "#ffffff",
